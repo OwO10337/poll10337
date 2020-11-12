7ba2e2c34bd9af008b05aa05dc6fe2c8e698fcf1
OwO <10930228@dcsh.tp.edu.tw>
2020-11-12 10:36:17 +0800

    投票主題列表、檢視、投票.....完成

--- default/views.py
+++ default/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Poll, Option
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+# def poll_list(req):
+#   polls = models.Poll.objects.all()
+#   return render(req, 'poll_list.html', {'polls_list': polls})
+
+class PollList(ListView):
+    model = Poll
+
+class PollDetail(DetailView):
+    model = Poll 
+
+    def get_context_date(self, **kwargs):
+        ctx = super().get_context_data(**kwagrs)
+        options = Option.objects.filter(poll_id=self. kwargs['pk'])
+        ctx['option_list'] = options 
+        return ctx 
+
+class PollVote(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        option = Option.objects. get(id=self. kwargs['old'])
+        option.count += 1 
+        option.save()
+        return '/poll/{}/'.format(option.poll id)
+       # return '/poll/'+str(option.poll_id)+'/'
