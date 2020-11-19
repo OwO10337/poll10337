b0e2a0893ccba037ca166cb78e82926f8d56d859
OwO <10930228@dcsh.tp.edu.tw>
2020-11-19 11:43:17 +0800

    投票主題新增，修改，刪除

--- default/urls.py
+++ default/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PollList, PollDetail
+from .views import *
 
 urlpatterns = [
     #path('poll/', views.poll_list),
     path('poll/', PollList.as_view()),
     path('poll/<int:pk>/', PollDetail.as_view()),
-    path('option/<int:oid>/', PollVote.as_view(),)
+    path('option/<int:oid>/', PollVote.as_view()),
+    path('poll/create/', PollCreate.as_view()),
+    path('poll/<int:pk>/edit/',PollEdit.as_view()),
+    path('poll/<int:pk>/delete/',PollDelete.as_view()),
 ]
--- default/views.py
+++ default/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Poll, Option
-from django.views.generic import ListView, DetailView
+from django.views.generic import *
 
 # Create your views here.
 # def poll_list(req):
@@ -13,16 +13,31 @@ class PollList(ListView):
 class PollDetail(DetailView):
     model = Poll 
 
-    def get_context_date(self, **kwargs):
-        ctx = super().get_context_data(**kwagrs)
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
         options = Option.objects.filter(poll_id=self. kwargs['pk'])
+        print(options)
         ctx['option_list'] = options 
         return ctx 
 
 class PollVote(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        option = Option.objects. get(id=self. kwargs['old'])
+    def get_redirect_url(self, * args, ** kwargs):
+        option = Option.objects. get(id=self. kwargs['oid'])
         option.count += 1 
         option.save()
-        return '/poll/{}/'.format(option.poll id)
+        return '/poll/{}/'.format(option.poll_id)
        # return '/poll/'+str(option.poll_id)+'/'
+
+class PollCreate(CreateView):
+    model = Poll
+    fields = ['subject', 'description']
+    success_url = '/poll/'
+
+class PollEdit(UpdateView):
+    model = Poll
+    fields = '__all__'
+    success_url = '/poll/'
+class PollDelete(DeleteView):
+    model = Poll
+    success_url = '/poll/'
+
